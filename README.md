81b066311e8f3ae87be4ab38edebf9f1928b8845
jorgeav527 <jorgeav527@gmail.com>
2024-10-28 19:48:39 -0500

    feat: developing in dev, adding context to the index function

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ```bash
 virtualenv .venv
 source .venv/Scripts/activate
-pip install -r requirements.txt
+pip install -r requirements.dev.txt
 ```
--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("base.html")
+    context = {"name": "jorge"}
+    return render_template(template_name_or_list="base.html", context=context)
+
 
 @app.route("/home", methods=["GET"])
 def home():
     return render_template("home.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
