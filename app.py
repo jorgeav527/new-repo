2d8fdda0b53b21fd5e6aaad4b3b01e1883acf6c4
jorgeav527 <jorgeav527@gmail.com>
2024-10-28 19:14:26 -0500

    feat: adding the template files

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
+
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template("home.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
