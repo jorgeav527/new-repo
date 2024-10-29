f63c7ee1200829d4175f7078a3151bbfd75f9db5
jorgeav527 <jorgeav527@gmail.com>
2024-10-28 20:29:57 -0500

    feat: adding jynja variables

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    context = {"name": "BASE"}
-    return render_template(template_name_or_list="base.html", context=context)
+    context = {
+        "name": "BASE",
+        "project": "Web developer Flask",
+    }
+    return render_template(template_name_or_list="base.html", **context)
 
 
 @app.route("/home", methods=["GET"])
 def home():
     context = {"name": "HOME"}
-    return render_template(template_name_or_list="home.html", context=context)
+    return render_template(template_name_or_list="home.html", **context)
 
 
 if __name__ == "__main__":
