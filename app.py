64245dfbd507c77d752a24ad658a61b612671cbb
jorgeav527 <jorgeav527@gmail.com>
2024-10-28 22:10:35 -0500

    feat: adding database and endpoint to fetchall posts

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template
+import sqlite3
+
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 app = Flask(__name__)
 
@@ -19,6 +27,20 @@ def home():
         "my_name": "Jorge",
     }
     return render_template(template_name_or_list="home.html", **context)
+
+
+@app.route("/post", methods=["GET"])
+def get_all_post():
+    conn = get_db_connection()
+    posts = conn.execute("SELECT * FROM posts").fetchall()
+    conn.close()
+    for post in posts:
+        print("======>", post["id"])
+        print("======>", post["title"])
+        print("======>", post["content"])
+        print("======>", post["created"])
+        print("====================================")
+    return render_template(template_name_or_list="posts.html")
 
 
 if __name__ == "__main__":
