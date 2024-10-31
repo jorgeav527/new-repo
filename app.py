96d9b7560fd35cf61a7b23531bc14f23acba6138
jorgeav527 <jorgeav527@gmail.com>
2024-10-30 20:54:13 -0500

    feat: create end point

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request, redirect, url_for
 import sqlite3
 
 
@@ -25,6 +25,7 @@ def home():
     context = {
         "name": "HOME",
         "my_name": "Jorge",
+        "list_languages": ["python", "go", "rust", "js", "ruby"],
     }
     return render_template(template_name_or_list="home.html", **context)
 
@@ -34,13 +35,41 @@ def get_all_post():
     conn = get_db_connection()
     posts = conn.execute("SELECT * FROM posts").fetchall()
     conn.close()
-    for post in posts:
-        print("======>", post["id"])
-        print("======>", post["title"])
-        print("======>", post["content"])
-        print("======>", post["created"])
-        print("====================================")
-    return render_template(template_name_or_list="posts.html", posts=posts)
+    return render_template(template_name_or_list="post/posts.html", posts=posts)
+
+
+@app.route("/post/<int:post_id>", methods=["GET"])
+def get_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return render_template(template_name_or_list="post/post.html", post=post)
+
+
+@app.route("/post/create", methods=["GET", "POST"])
+def create_one_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute(
+            "INSERT INTO posts (title, content) VALUES (?, ?)",
+            (title.upper(), content.capitalize()),
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for("get_all_post"))
+    elif request.method == "GET":
+        return render_template("post/create.html")
+
+    # conn = get_db_connection()
+    # post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    # conn.close()
+    # if post is None:
+    #     abort(404)
+    # return render_template(template_name_or_list="post/post.html", post=post)
 
 
 if __name__ == "__main__":
