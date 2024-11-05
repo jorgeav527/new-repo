c6d83fc14f17c804b43a249eea135b8a6b94c867
jorgeav527 <jorgeav527@gmail.com>
2024-11-04 19:47:03 -0500

    feat: adding delete endpoint

--- app.py
+++ app.py
@@ -93,5 +93,15 @@ def edit_one_post(post_id):
         return render_template(template_name_or_list="post/edit.html", post=post)
 
 
+@app.route("/post/delete/<int:post_id>", methods=["POST"])
+def delete_one_post(post_id):
+    conn = get_db_connection()
+    conn.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("get_all_post"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
